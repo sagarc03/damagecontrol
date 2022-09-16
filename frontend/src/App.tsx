37d60284a74c736cc07633c76cf3671cb91d0922
Sagar Chavan <sagar.c.03@gmail.com>
2022-09-15 22:21:22 -0400

    ci: set up local frontend development

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import 'tailwindcss/tailwind.css'
 import './App.css'
 
 function App() {
@@ -8,16 +9,15 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>Vite + React</h2>
-      <div className="card">
+      <div className="card font-bold">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
